6b4e0f742c3757b48288ce032a6bbf14cc1ce9d7
Budhaditya Mukhopadhyay <62721668+BudhaTronix@users.noreply.github.com>
2021-11-25 16:05:45 +0100

    Updated for pycharm

--- Codes/Pipeline_executer.py
+++ Codes/Pipeline_executer.py
@@ -1,72 +1,65 @@
 import glob
-from Pipeline import CUI
+from Pipeline import pipeline
 from keras.models import load_model
 
 
-def main():
-    model = load_model('final_model.h5', compile=False)
-    # Global Variable Declaration
-    use_GUI = True
-    SMOOTHING_RADIUS = 50
-    visual_area = 0  #
-    correctionFactor_Face = 30
-    correctionFactor_Lip = 30
-    threshold = 40
-    thresh_iterations = 2
-    disp = False
-    save_in_excel = True
-    time_slice = 5
-    MainFile = "C:\\Users\\budha\\PycharmProjects\\TongueFrequencyCalculator\\VideoFies\\37a.mov"  # Change When needed
-    # Change it to True if you want Stabilization
-    if use_GUI:
-        from GUI import GUI
-        obj = GUI(model)
-        obj.loaderFunc()
-
-    else:
-        Stabilize = False
-
+class pipelineExecutor:
+    def __init__(self):
+        self.model = load_model('final_model.h5', compile=False)
+        # Global Variable Declaration
+        self.use_GUI = True
+        self.SMOOTHING_RADIUS = 50
+        self.visual_area = 0  #
+        self.correctionFactor_Face = 30
+        self.correctionFactor_Lip = 30
+        self.threshold = 40
+        self.thresh_iterations = 2
+        self.disp = False
+        self.save_in_excel = True
+        self.time_slice = 5
+        # Change Base File Name
+        self.MainFile = "C:\\Users\\budha\\PycharmProjects\\TongueFrequencyCalculator\\VideoFies\\37a.mov"
+        # Change to Stabilize
+        self.Stabilize = False
         # Change it to True if you want Video to be compressed
-        Video_Compression = False
-
+        self.Video_Compression = False
         # Change it to True if you want Face Extraction
-        Face_Extract = True
-
+        self.Face_Extract = True
         # Change it to True if you want Tongue Extraction
-        Lip_Extraction = True
-
+        self.Lip_Extraction = True
         # Change it to True if you want to calculate the frequency
-        Frquency_Calculation = True
-
+        self.Frquency_Calculation = True
         # Change it to True if you want to run on All files
-        Multi_mode = True
+        self.Multi_mode = True
 
-        if Multi_mode:
-            for file_name in sorted(glob.iglob('*.mov')):
-                print("\n ################################################################# \n")
-                print("Working on File Name : ", file_name)
-                print("Processing Starts")
-                MainFile = file_name
-                CUI(MainFile, Stabilize, Video_Compression, Face_Extract, Lip_Extraction, Frquency_Calculation,
-                    threshold, thresh_iterations,
-                    visual_area, disp,
-                    correctionFactor_Face,
-                    correctionFactor_Lip,
-                    save_in_excel,
-                    time_slice,
-                    model)
-                print("Processing Ends")
-                print("\n ################################################################# \n")
+    def main(self):
+        # Change it to True if you want Stabilization
+        if self.use_GUI:
+            from GUI import GUI
+            obj = GUI(self.model)
+            obj.loaderFunc()
+
         else:
-            CUI(MainFile, Stabilize, Video_Compression, Face_Extract, Lip_Extraction, Frquency_Calculation,
-                threshold, thresh_iterations,
-                visual_area, disp,
-                correctionFactor_Face,
-                correctionFactor_Lip,
-                save_in_excel,
-                time_slice,
-                model,SMOOTHING_RADIUS)
+            obj = pipeline()
+            if self.Multi_mode:
+                for file_name in sorted(glob.iglob('*.mov')):
+                    print("\n ################################################################# \n")
+                    print("Working on File Name : ", file_name)
+                    print("Processing Starts")
+                    obj.CUI(file_name, self.Stabilize, self.Video_Compression, self.Face_Extract, self.Lip_Extraction,
+                            self.Frquency_Calculation, self.threshold, self.thresh_iterations, self.visual_area,
+                            self.disp,
+                            self.correctionFactor_Face, self.correctionFactor_Lip,
+                            self.save_in_excel, self.time_slice, self.model, self.SMOOTHING_RADIUS)
+                    print("Processing Ends")
+                    print("\n ################################################################# \n")
+            else:
+                obj.CUI(self.MainFile, self.Stabilize, self.Video_Compression, self.Face_Extract, self.Lip_Extraction,
+                        self.Frquency_Calculation, self.threshold, self.thresh_iterations, self.visual_area, self.disp,
+                        self.correctionFactor_Face, self.correctionFactor_Lip,
+                        self.save_in_excel, self.time_slice, self.model, self.SMOOTHING_RADIUS)
 
 
-# if __name__ == "__main__":
-main()
+execution = pipelineExecutor()
+execution.main()
+
